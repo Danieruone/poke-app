919641ea87310f08f5fb7a616fd6f50094711b74
Daniel Mendoza <danieruone@gmail.com>
2024-03-11 20:33:41 -0500

    :sparkles: Implement pokemo fetch method

--- lib/presentation/providers/pokemon_provider.dart
+++ lib/presentation/providers/pokemon_provider.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:poke_app/config/helpers/get_pokemon_list.dart';
+import 'package:poke_app/domain/entities/pokemon_entity.dart';
 
-class PokemonProvider extends ChangeNotifier {}
+class PokemonProvider extends ChangeNotifier {
+  List<PokemonEntity> pokemonList = [];
+
+  PokemonProvider() {
+    getPokemonList();
+  }
+
+  Future<List<PokemonEntity>> getPokemonList() async {
+    final responseList = GetPokemonList().getList();
+    // TODO return pokemon list entity
+    return [];
+  }
+}
--- lib/presentation/screens/pokemon_screen.dart
+++ lib/presentation/screens/pokemon_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:poke_app/presentation/providers/pokemon_provider.dart';
+import 'package:provider/provider.dart';
 
 class PokemonScreen extends StatelessWidget {
   const PokemonScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final pokemonProvider = context.watch<PokemonProvider>();
     return const Placeholder();
   }
 }
